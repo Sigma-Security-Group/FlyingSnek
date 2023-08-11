6df97cb4356fbe149e862240b4130e145c478dcf
adesanjo <jesuisanonime@gmail.com>
2023-08-11 12:34:23 +0200

    temporary channel delete

--- cogs/duels.py
+++ cogs/duels.py
@@ -24,6 +24,11 @@ class Duels(commands.Cog):
     async def on_ready(self) -> None:
         log.debug("Cog ready: Duels", flush=True)
         cogsReady["duels"] = True
+        
+        channels = self.bot.get_guild(GUILD).channels
+        for channel in channels:
+            if channel.name.startswith("σad--aratam-vs-") or channel.name.startswith("322nd-sqdn--aspire-vs-"):
+                await channel.delete()
 
     @app_commands.command(name="challenge", description="Challenge a user to a duel")
     @app_commands.guilds(GUILD)
